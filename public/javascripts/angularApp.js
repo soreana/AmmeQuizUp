ec3e87c1c360a4e486c9af123a5cae6825598b6d
soreana <esterlinkof@gmail.com>
2016-02-18 15:54:31 +0330

    finished Wiring Everything Up

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,15 +1,47 @@
 var app = angular.module('flapperNews', ['ui.router']);
 
-app.factory('posts', [function () {
+app.factory('posts', [ '$http' , function ( $http) {
     var o = {
         posts: [
-            {title: 'post 1', upvotes: 5},
-            {title: 'post 2', upvotes: 2},
-            {title: 'post 3', upvotes: 15},
-            {title: 'post 4', upvotes: 9},
-            {title: 'post 5', upvotes: 4}
+            {title: 'example post : posts didn\'t load' , upvotes: 16},
         ]
     };
+	
+	o.getAll = function() {
+    	return $http.get('/posts').success(function(data){
+      		angular.copy(data, o.posts);
+    	});
+  	};
+	
+	o.creat = function(post){
+		return $http.post('/posts', post).success( function(data){
+			o.posts.push( data );
+		});
+	};
+	
+	o.upvote = function( post ){
+		return $http.put('/posts/' + post._id + '/upvote')
+			.success( function( data ){
+				post.upvotes += 1;
+			});
+	};
+	
+	o.get = function( id ){
+		return $http.get( '/posts/' + id ).then( function(res){
+			return res.data;
+		});
+	};
+	
+	o.addComment = function( id , comment ){
+		return $http.post( '/posts/' + id + '/comments' , comment );
+	};
+	
+	o.upvoteComment = function( post , comment ){
+		return $http.put( '/posts/' + post._id + '/comments/' + comment._id + '/upvote' )
+			.success( function(data){
+				comment.upvotes += 1;
+			});
+	};
 
     return o;
 }]);
@@ -23,32 +55,49 @@ app.config([
             .state( 'home' ,{
                 url: '/home' ,
                 templateUrl: '/home.html',
-                controller: 'MainCtrl'
+                controller: 'MainCtrl' ,
+				resolve: {
+    				postPromise: ['posts', function(posts){
+      					return posts.getAll();
+    				}]
+  				}
             });
 		$stateProvider
 			.state( 'posts', {
 				url: '/posts/{id}' ,
 				templateUrl: '/posts.html' ,
-				controller: 'PostsCtrl'
+				controller: 'PostsCtrl',
+				resolve: {
+					post: [ '$stateParams' , 'posts' , function( $stateParams , posts ){
+						return posts.get( $stateParams.id );
+					}]
+				}
 			});
 			
         $urlRouterProvider.otherwise('home');
 }]);
 
 app.controller('PostsCtrl',
-		['$scope', '$stateParams', 'posts',
-		function ($scope , $stateParams , posts){
-			$scope.post = posts.posts[$stateParams.id];
+		['$scope', 'posts','post',
+		function ($scope , posts , post){
+			$scope.post = post;
 			$scope.addComment = function(){
 				if( ! $scope.body || $scope.body === '' ) return;
 				if( ! $scope.post.comments  ){
 					$scope.post.comments = [] ;
 				}
-				$scope.post.comments.push( { author: 'user', body: $scope.body , upvotes: 0});
+				
+				posts.addComment( post._id , {
+					body: $scope.body,
+					author: 'user'
+				}).success( function(comment){
+					$scope.post.comments.push( comment );
+				});
+				
 				$scope.body = ''; 
 			};
 			$scope.incrementUpvotes = function( comment ){
-				comment.upvotes += 1;
+				posts.upvoteComment( post , comment );
 			};
 }]);
 
@@ -60,19 +109,16 @@ app.controller('MainCtrl',
             $scope.addPost = function () {
                 if ( ! $scope.title || $scope.title === '')
                     return;
-                $scope.posts.push({
+				
+				posts.creat({
                     title: $scope.title,
                     link: $scope.link,
-                    upvotes: 0,
-					comments: [
-    					{author: 'Joe', body: 'Cool post!', upvotes: 0},
-    					{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-  					]
-                });
+				});
+				
                 $scope.title = '';
                 $scope.link = '';
             };
             $scope.incrementUpvotes = function (post) {
-                post.upvotes += 1;
+                posts.upvote( post ) ;
             };
         }]);
