a03aaca733892a76e79bf2eaea59ef9243545a40
soreana <esterlinkof@gmail.com>
2016-05-10 17:52:00 +0430

    change angular module name

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,4 +1,4 @@
-var app = angular.module('flapperNews', ['ui.router']);
+var app = angular.module('AmooQuiz', ['ui.router']);
 
 app.factory('posts', ['$http', 'auth', function ($http, auth) {
     var o = {
