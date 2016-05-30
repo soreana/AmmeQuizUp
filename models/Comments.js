b74c17b108b053370c8acd6f5711b441b1d010ff
soreana <esterlinkof@gmail.com>
2016-05-29 21:34:44 -0700

    add answer to comment Schema

--- models/Comments.js
+++ models/Comments.js
@@ -3,8 +3,8 @@ var mongoose = require('mongoose');
 var CommentSchema = new mongoose.Schema({
 	body: String,
 	author: String,
-	upvotes: {type: Number , default:0 },
-	post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' }
+	post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' },
+	choices:[{title:String,answer:Boolean}]
 });
 
 CommentSchema.methods.upvote = function( cb ){
